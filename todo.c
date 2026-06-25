f0ca5784c547bf21b22a79a8309c64378642d4b0
ronbarrantes <ronbarrantes@gmail.com>
2026-06-25 12:23:15 -0400

    Add todo plan

---
+++ todo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// READ/WRITE Mechanism
+// should be able to read and write from anywhere
+//
+// it can be like a csv
+// id, is_completed, task
+
+// CRUD Mechanism
+// use for the inputs and pass the values to the correct read/write
+
+// Start and Stop
+// go into some kind of loop
+
+int main(void) {
+  printf("Todo app");
+  return 0;
+}
