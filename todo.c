b841cd9d63100dae649d28c795095d1f8ad918a8
ronbarrantes <ronbarrantes@gmail.com>
2026-06-26 16:27:53 -0400

    Learn to open a file

--- todo.c
+++ todo.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
+// #include <stdlib.h>
+
+// INIT fn
+// Check if file exist
+
+int init() {
+  FILE *filePointer;
+  // check if file exist
+  //
+  //
+  // if it doesn't then create it
+  filePointer = fopen("file.csv", "r");
+
+  if (filePointer == NULL) {
+    printf("Did not open file\n");
+    return 1;
+  }
+
+  fclose(filePointer);
+  return 0;
+}
 
 // READ/WRITE Mechanism
 // should be able to read and write from anywhere
-//
 // it can be like a csv
 // id, is_completed, task
 
@@ -13,6 +33,10 @@
 // go into some kind of loop
 
 int main(void) {
-  printf("Todo app");
+  if (!init()) {
+    printf("Opened the file well\n");
+  };
+
+  printf("Todo app\n");
   return 0;
 }
